2948c1d63c1e259b63d9244d1ceeb21f0127ac6b
j4m1n-t <j4m1n_t@pm.me>
2024-07-22 09:17:11 -0500

    Revert "CRUD -> crud"
    This reverts commit d0fe946f3cb4f05a65abf7786a3a1d22bd03957d.

--- pkg/layouts/auditsTab.go
+++ pkg/layouts/auditsTab.go
@@ -9,7 +9,7 @@ import (
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 
-	crud "github.com/j4m1n-t/goAudit/pkg/crud"
+	crud "github.com/j4m1n-t/goAudit/pkg/CRUD"
 	state "github.com/j4m1n-t/goAudit/pkg/status"
 )
 
